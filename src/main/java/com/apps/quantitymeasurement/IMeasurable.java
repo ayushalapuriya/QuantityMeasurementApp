c9f401465192ba646c77f396d8c05899acf1f77a
Ayush Alapuriya <alapuriya0@gmail.com>
2026-03-11 11:34:36 +0530

    [Ayush Alapuriya] Refactor. Update solution of QuantityMeasurementApp UC14

--- src/main/java/com/apps/quantitymeasurement/IMeasurable.java
+++ src/main/java/com/apps/quantitymeasurement/IMeasurable.java
@@ -2,15 +2,22 @@ package com.apps.quantitymeasurement;
 
 public interface IMeasurable {
 
-    double getConversionFactor();
+    // Mandatory Conversion Methods
+    double convertToBaseUnit(double value);
 
-    default double convertToBaseUnit(double value) {
-        return value * getConversionFactor();
-    }
-
-    default double convertFromBaseUnit(double baseValue) {
-        return baseValue / getConversionFactor();
-    }
+    double convertFromBaseUnit(double baseValue);
 
     String getUnitName();
+
+    // UC14 OPTIONAL ARITHMETIC SUPPORT (DEFAULT METHODS)
+    SupportsArithmetic supportsArithmetic = () -> true;
+
+    default boolean supportsArithmetic() {
+        return supportsArithmetic.isSupported();
+    }
+
+    // operation validation hook
+    default void validateOperationSupport(String operation) {
+        // Default → all operations allowed
+    }
 }
