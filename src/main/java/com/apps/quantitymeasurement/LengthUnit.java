08a1023e40b836119c46677950756835be0d1d82
Ayush Alapuriya <121999959+ayushalapuriya@users.noreply.github.com>
2026-03-18 16:44:28 +0530

    [Ayush Alapuriya] Refactor. Update LengthUnit
    Added LengthUnit enum with conversion methods for various length units.

--- src/main/java/com/apps/quantitymeasurement/LengthUnit.java
+++ src/main/java/com/apps/quantitymeasurement/LengthUnit.java
@@ -1,5 +1,30 @@
 package com.apps.quantitymeasurement;
 
-public class LengthUnit {
+public enum LengthUnit implements Unit {
 
+    FEET(1.0),
+    INCHES(1.0 / 12.0),
+    YARDS(3.0),
+    CENTIMETERS(1.0 / 30.48);
+
+    private final double factorToFeet;
+
+    LengthUnit(double factorToFeet) {
+        this.factorToFeet = factorToFeet;
+    }
+
+    @Override
+    public double toBase(double value) {
+        return value * factorToFeet;
+    }
+
+    @Override
+    public double fromBase(double baseValue) {
+        return baseValue / factorToFeet;
+    }
+
+    @Override
+    public String getCategory() {
+        return "LENGTH";
+    }
 }
