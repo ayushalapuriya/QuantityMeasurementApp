ab470db0e21b4e3b6f299947bb80a766897aae9c
Ayush Alapuriya <121999959+ayushalapuriya@users.noreply.github.com>
2026-03-30 17:25:43 +0530

    Delete SpringBootIntegration directory

--- SpringBootIntegration/src/main/java/com/app/quantitymeasurement/config/ApplicationConfig.java
+++
@@ -1,7 +0,0 @@
-package com.app.quantitymeasurement.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ApplicationConfig {
-}
--- SpringBootIntegration/src/main/java/com/app/quantitymeasurement/repository/IQuantityMeasurementRepository.java
+++
@@ -1,8 +0,0 @@
-package com.app.quantitymeasurement.repository;
-
-import com.app.quantitymeasurement.entity.QuantityMeasurementEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IQuantityMeasurementRepository
-        extends JpaRepository<QuantityMeasurementEntity, Long> {
-}
