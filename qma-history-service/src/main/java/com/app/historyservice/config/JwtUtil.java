5ca9ea6dccf3b234dab6157a28af489a98a2e06b
Ayush Alapuriya <alapuriya0@gmail.com>
2026-04-17 15:29:53 +0530

    [Ayush Alapuriya] Add. Develop a microservices architecture

---
+++ qma-history-service/src/main/java/com/app/historyservice/config/JwtUtil.java
@@ -0,0 +1,33 @@
+
+package com.app.historyservice.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
